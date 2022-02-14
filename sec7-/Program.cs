752f4293bade6b1c5521ce186a8d9d34ea93a861
ezu0 <h-ezure@est.co.jp>
2022-02-14 15:58:40 +0900

    oop

--- sec7-/Program.cs
+++ sec7-/Program.cs
@@ -1,7 +1,27 @@
-﻿class Hello
+﻿using System;
+
+namespace ClassSample
 {
-    static void Main()
+    class Program
     {
-        System.Console.WriteLine("Hello Worlddd!!!!");
+        static void Main()
+        {
+            var book = new Book();
+            book.Title = "吾輩は猫である";
+            book.Author = "夏目漱石";
+            book.Pages = 610;
+            book.Rating = 4;
+            Console.WriteLine($"書籍名：{book.Title}");
+            Console.WriteLine($"著者名：{book.Author}");
+            Console.WriteLine($"ページ数：{book.Pages}");
+            Console.WriteLine($"評価：{book.Rating}");
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
     }
 }
